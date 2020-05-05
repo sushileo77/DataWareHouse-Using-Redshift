10a74f4d91608865819dda10cb4e828a36e06a39
Sushant Sakhalkar <sushant.sakhalkar@gmail.com>
2020-05-05 10:33:05 -0700

    Add files via upload
    Updates After Code Review

--- sql_queries.py
+++ sql_queries.py
@@ -39,7 +39,7 @@ staging_events_table_create= ("""
                 registration  NUMERIC ,
                 sessionId     INTEGER ,
                 song          VARCHAR ,
-                status        INT NOT ,
+                status        INT ,
                 ts            NUMERIC ,
                 userAgent     VARCHAR ,
                 userId        INT 
@@ -67,8 +67,8 @@ songplay_table_create = ("""
         start_time timestamp,
         user_id int NOT NULL REFERENCES users(user_id), 
         level VARCHAR,
-        song_id VARCHAR REFERENCES songs(song_id),
-        artist_id VARCHAR REFERENCES artists(artist_id) ,
+        song_id VARCHAR NOT NULL REFERENCES songs(song_id),
+        artist_id VARCHAR NOT NULL REFERENCES artists(artist_id) ,
         session_id INT,
         location VARCHAR,
         user_agent VARCHAR
@@ -106,7 +106,7 @@ artist_table_create = ("""
 """)
 
 time_table_create = ("""
-    create table times (
+    create table time (
         start_time numeric primary key,
         hour int  ,
         day int   ,
@@ -222,7 +222,7 @@ time_table_insert = ("""
 """)
 
 # QUERY LISTS
-create_table_queries  = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries  = [staging_events_table_create, staging_songs_table_create,user_table_create, song_table_create, artist_table_create, time_table_create,songplay_table_create]
 drop_table_queries    = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries    = [staging_events_copy, staging_songs_copy]
 insert_table_queries  = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
